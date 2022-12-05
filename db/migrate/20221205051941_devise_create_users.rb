2c402ace63eba6fdb26821cdc9864ae5628cf5b4
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-05 14:29:23 +0900

    userテーブルmigrate各カラム名追記

--- db/migrate/20221205051941_devise_create_users.rb
+++ db/migrate/20221205051941_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :family_name_kanji,  null: false
+      t.string :first_name_kanji,   null: false
+      t.string :family_name_kana,   null: false
+      t.string :first_name_kana,    null: false 
+      t.date   :day_of_birth,       null: false
 
       ## Recoverable
       t.string   :reset_password_token
