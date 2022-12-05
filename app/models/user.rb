f6a13d6cb905d67a2a5a3acfdbf47c617743de9d
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-05 17:26:52 +0900

    パスワード最小6文字にバリテーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validate
+
+  validates :encrypted_password, presence: true, length: { mimimum: 6 }
 end
