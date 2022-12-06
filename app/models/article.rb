c89237a6427ed515ab35aa60b79049e4af542339
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-06 18:48:54 +0900

    articl.モデル、バリテーション

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,22 @@ class Article < ApplicationRecord
   belongs_to :postage
   belongs_to :area
   belongs_to :send
+
+  #空の投稿を保存できないようにする
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, presence: true
+  validates :postage_id, presence: true
+  validates :area_id, presence: true
+  validates :send_id, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :postage_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :send_id, numericality: { other_than: 1 }
+
 end
 
 
