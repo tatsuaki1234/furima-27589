23defd9926a413ea91c851d971fd21b0334460b2
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-06 18:03:25 +0900

    item params生成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,13 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new(item_params)
+  end
 
 
   private
   
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:nickname, :content, :category_id, :condition_id, :postage_id, :area_id, :send_id, :price, :image).merge(user_id: current_user.id)
   end
 end
