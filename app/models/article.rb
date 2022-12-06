48bd0a4a31a7a29dde7f1a8621a40a25ec5042c2
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-06 18:19:01 +0900

    article.rb　アソシエーション

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :area
+  belongs_to :send
+end
+
+
