c11ccf56eda893b953f936f2432d2dbc29a68453
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-06 17:37:09 +0900

    item migrationファイルカラム追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
 
   private
   
-  def message_params
+  def item_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
--- db/migrate/20221206081030_create_items.rb
+++ db/migrate/20221206081030_create_items.rb
@@ -1,8 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :nickname,           null: false 
+      t.text :content,              null: false
+      t.integer :category_id,       null: false
+      t.integer :condition_id,      null: false
+      t.integer :postage_id,        null: false
+      t.integer :area_id,           null: false
+      t.integer :send_id,           null: false
+      t.integer :price,             null: false  
+      t.references :user,           null: false, foreign_key: true
       t.timestamps
     end
   end
 end
+
