b2272903c5462216852dde8f4ee486c68bfa451b
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-11 03:06:17 +0900

    編集urlを直接入力しても遷移しない設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -27,20 +28,23 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to  root_path
+    end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_update_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
   
   def item_params
-    params.require(:item).permit(:name, :image, :content, :category_id, :condition_id, :postage_id, :area_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
-  end
-
-  def item_update_params
     params.require(:item).permit(:name, :image, :content, :category_id, :condition_id, :postage_id, :area_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
