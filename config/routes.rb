818a6a2d81942b32a7e5794b3ff9cb8db8ec07f1
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-10 23:51:34 +0900

    ルーティングにeditを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'items/index'
   root to: "items#index"
-  resources :items,     only: [:index, :new, :create, :show] 
+  resources :items,     only: [:index, :new, :create, :show, :edit] 
 end
 
 
