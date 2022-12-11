9b4be1135f1b509d085519227a6c4d69ee31bc0e
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-12 01:31:31 +0900

    addressマイグレーション記述、実行

--- db/migrate/20221211152532_create_addresses.rb
+++ db/migrate/20221211152532_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :post_code,     null: false 
+      t.string     :city_code,     null: false
+      t.integer    :area_id,       null: false
+      t.string     :post_number,   null: false
+      t.string     :building_name, null: false
+      t.string     :phone_number,  null: false
+      t.references :order,          null: false, foreign_key: true
       t.timestamps
     end
   end
