819ab53a35a81c304667d965e4334412bafa408c
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-11 18:22:25 +0900

    destroyルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items,     only: [:index, :new, :create, :show, :edit, :update] 
+  resources :items,     only: [:index, :new, :create, :show, :edit, :update, :destroy] 
 end
 
 
 
+# item DELETE /items/:id(.:format)                        items#destroy
