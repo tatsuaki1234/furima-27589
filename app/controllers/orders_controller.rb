3ad4c0df3ac0af859fb15dc0890604e103ac7768
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2022-12-13 20:43:58 +0900

    payアクションをprivateアクションにまとめた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,8 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = "sk_test_bfdb866e7228ca3b383cd97c"  
-      Payjp::Charge.create( amount: @item.price, card: order_params[:token], currency: 'jpy' )
+      order_payment
       @order_address.save
       redirect_to root_path
     else
@@ -22,6 +21,11 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:post_code, :city_code, :area_id, :post_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def order_payment
+    Payjp.api_key = "sk_test_bfdb866e7228ca3b383cd97c"  
+    Payjp::Charge.create( amount: @item.price, card: order_params[:token], currency: 'jpy' )
   end
 end
 
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     post_number      {'2-33-6'}
     phone_number     {'09084763526'}
     building_name    {'おおきにマンション'}
+    token            {"tok_abcdefghijk00000000000000000"}
     association :user
     association :item
     
