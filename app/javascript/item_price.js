89bf9be7ff83d25dda95175092358a6ecb2ced98
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-07 17:13:59 +0900

    商品出品の値段JS編集

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,26 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
+// window.addEventListener('load', () => {
+//   const priceInput = document.getElementById("item-price");
   
 
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
+//   priceInput.addEventListener("input", () => {
+//     const inputValue = priceInput.value;
 
-    const addTaxDom = document.getElementById("add-tax-price")
-    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+//     const addTaxDom = document.getElementById("add-tax-price")
+//     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
 
-    const profit = document.getElementById("profit")
-    profit.innerHTML = (Math.floor(inputValue - inputValue * 0.1));
-})
-})
+//     const profit = document.getElementById("profit")
+//     profit.innerHTML = (Math.floor(inputValue - inputValue * 0.1));
+// })
+// })
 
+
+
+    window.addEventListener('load', () => {
+      const priceInput = document.getElementById("item-price");
+       priceInput.addEventListener("input", () => {
+         const addTaxDom = document.getElementById("add-tax-price");
+         addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+         const addPofitDom = document.getElementById("profit");
+         addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
+     })
+    });
